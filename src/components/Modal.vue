4f716408a6a57d0f79163dca2b6e5b5aabba9dd6
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-18 10:19:07 -0300

    Modal project created with Vue CLI

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.modal {
+    width: 25rem;
+    padding: 1.25rem;
+    margin: 6.25rem auto;
+    background: white;
+    border-radius: .625rem;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+}
+h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+
+}
+</style>
